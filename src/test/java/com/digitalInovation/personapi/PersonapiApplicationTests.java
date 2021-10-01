cb2704bf7b00117ac237825c0d889f76f4a69c12
Luis.Paes <luis.paes2@etec.sp.gov.br>
2021-10-01 13:06:00 -0300

    add system properties

---
+++ src/test/java/com/digitalInovation/personapi/PersonapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalInovation.personapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
